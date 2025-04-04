b63416e2f1c9ab6d0603240fd3a6ae81f61b25b3
hrrs01 <haavard.hn@gmail.com>
2025-04-04 17:57:32 +0200

    Added helper function to split Can IDs as documented by DTI

--- inc/utils/alignutils.h
+++ inc/utils/alignutils.h
@@ -2,11 +2,13 @@
 #define ALIGN_UTILS_H
 
 #include "stdint.h"
-#include "align.h"
+#include "stdbool.h"
+#include "tim.h"
 
 // Simple delay function that takes microseconds 
 // Configured around TIM2 with a 1MHz signal, with a overflow reset on UINT32_MAX
 
 void Align_DelayUs(uint32_t us);
+void Align_SplitCanId(uint32_t can_id, uint8_t *packet_id, uint8_t *node_id, bool is_extended);
 
 #endif
--- src/alignutils.c
+++ src/alignutils.c
@@ -13,3 +13,13 @@ void Align_DelayUs(uint32_t us){
   while(htim2.Instance->CNT < target){}
 
 }
+
+void Align_SplitCanId(uint32_t can_id, uint8_t *packet_id, uint8_t *node_id, bool is_extended){
+  if(is_extended){
+    *packet_id = (can_id >> 8);
+    *node_id = can_id & 0xFF;
+  }else{
+    *packet_id = (can_id >> 5);
+    *node_id = can_id & 0x1F;
+  }
+}
